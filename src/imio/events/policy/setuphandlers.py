37a7708f2512a0897fafa216dd82ac7f4da4851f
Christophe Boulanger <christophe.boulanger@imio.be>
2022-02-01 11:53:20 +0100

    blacked files

--- src/imio/events/policy/setuphandlers.py
+++ src/imio/events/policy/setuphandlers.py
@@ -22,7 +22,7 @@ class HiddenProducts(object):
     def getNonInstallableProducts(self):
         """Hides profiles from QuickInstaller"""
         return [
-            u"imio.events.core",
+            "imio.events.core",
         ]
 
 
